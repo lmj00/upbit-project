c4d87a3b2b99c2d814d71960191f59f771f1214f
이민진 <e.minjin00@gmail.com>
2023-03-17 13:41:24 +0900

    Feat: getTicker scheduler 추가

--- coin/apps.py
+++ coin/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CoinConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'coin'
+    
+    def ready(self):
+        from . import scheduler
+        scheduler.start()
