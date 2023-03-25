6805ea431c486ae71c755e00990a14b2b25b2d08
이민진 <e.minjin00@gmail.com>
2023-03-25 16:52:19 +0900

    Feat: apps ready 변경

--- coin/apps.py
+++ coin/apps.py
@@ -4,8 +4,7 @@ from django.apps import AppConfig
 class CoinConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'coin'
-    
+
     def ready(self):
-        from coin.price import get_ticker
-        import asyncio
-        asyncio.run(get_ticker())
+        from .tasks import background_ticker
+        background_ticker()
