574db096a60ec984474e396372449602c307900c
이민진 <e.minjin00@gmail.com>
2023-03-31 04:31:01 +0900

    Feat: 매수 주문 추가
    volume(주문량)은 보유 원화 전액으로 매수

--- trade/order.py
+++ trade/order.py
@@ -30,3 +30,37 @@ def get_krw():
     res = requests.get(server_url + '/v1/accounts', params=params, headers=headers).json()
 
     return float(res[0]['balance'])
+
+
+def order_bid():
+    coin = get_top_trade_volume_coin()
+
+    params = {
+        'market': coin.code,
+        'side': 'bid',
+        'ord_type': 'limit',
+        'price': coin.trade_price,
+        'volume': (get_krw() - get_krw() * 0.0005) / coin.trade_price
+    }
+
+    query_string = unquote(urlencode(params, doseq=True)).encode("utf-8")
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorization = 'Bearer {}'.format(jwt_token)
+    headers = {
+    'Authorization': authorization, 
+    }
+
+    res = requests.post(server_url + '/v1/orders', json=params, headers=headers)
+    res.json()
