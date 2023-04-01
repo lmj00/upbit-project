dfd25f79f91dce96c515aa2898621ff12f0c05b4
이민진 <e.minjin00@gmail.com>
2023-04-01 22:16:18 +0900

    Feat: 거래 예외 처리

--- trade/views.py
+++ trade/views.py
@@ -1,18 +1,24 @@
+from django.contrib import messages
 from django.shortcuts import render
 from coin.price import get_top_trade_volume_coin
 from trade.order import get_krw, order_bid
 
 
 def trade(request):
-    if request.method == 'POST':
-        if get_krw() >= 5002.5:
-            order_bid()
-        else:
-            print("최소 주문 금액이 부족합니다.")
+    try: 
+        if request.method == 'POST':
+            if get_krw() >= 5002.5:
+                order_bid()
 
-                
+    except:
+        messages.add_message(
+            request, 
+            messages.INFO, 
+            'Open API Key 관리에서 허용 IP주소를 확인해주세요.'
+        )
+
     context = {
         'max_volume_coin': get_top_trade_volume_coin()
     }
 
-    return render(request, 'trade/test.html', context=context)
+    return render(request, 'trade/trade.html', context=context)
