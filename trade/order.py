be3668478373aaa799504d2e0fa33b55546c5445
이민진 <e.minjin00@gmail.com>
2023-04-03 04:09:08 +0900

    Feat: order_cancel 추가

--- trade/order.py
+++ trade/order.py
@@ -94,3 +94,30 @@ def get_last_order_value():
     res = requests.get(server_url + '/v1/orders', params=params, headers=headers).json()
 
     return res[0]
+
+
+def order_cancel(uuid):
+    params = {
+    'uuid': uuid
+    }
+
+    query_string = unquote(urlencode(params, doseq=True)).encode("utf-8")
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorization = 'Bearer {}'.format(jwt_token)
+    headers = {
+    'Authorization': authorization,
+    }
+
+    res = requests.delete(server_url + '/v1/order', params=params, headers=headers)
