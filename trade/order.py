1277a419e3445bb76f7c0832933b37b5b4f07abb
이민진 <e.minjin00@gmail.com>
2023-04-03 03:53:20 +0900

    Feat: get_last_order_value 추가
    uuid 값을 비교하기 위해 마지막 주문 값을 가져옴

--- trade/order.py
+++ trade/order.py
@@ -1,5 +1,6 @@
 from urllib.parse import urlencode, unquote
 from coin.price import get_top_trade_volume_coin
+
 import jwt
 import hashlib
 import os
@@ -64,3 +65,32 @@ def order_bid():
 
     res = requests.post(server_url + '/v1/orders', json=params, headers=headers)
     res.json()
+
+
+def get_last_order_value():
+    params = {
+    'states[]': ['done', 'cancel']
+    }
+
+    query_string = unquote(urlencode(params, doseq=True)).encode("utf-8")
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorization = 'Bearer {}'.format(jwt_token)
+    headers = {
+    'Authorization': authorization,
+    }
+
+    res = requests.get(server_url + '/v1/orders', params=params, headers=headers).json()
+
+    return res[0]
