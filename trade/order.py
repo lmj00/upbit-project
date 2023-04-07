fd9009f4e3372025b45616836004d693d7112f59
이민진 <e.minjin00@gmail.com>
2023-04-07 18:22:23 +0900

    Feat: get_complete_order_value 추가
    주문 완료, 취소 된 코인을 가져옴

--- trade/order.py
+++ trade/order.py
@@ -98,6 +98,35 @@ def get_wait_order_value():
     return res
 
 
+def get_complete_order_value():
+    params = {
+    'states[]': ['done', 'cancel']
+    }
+
+    query_string = unquote(urlencode(params, doseq=True)).encode("utf-8")
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, secret_key)
+    authorization = 'Bearer {}'.format(jwt_token)
+    headers = {
+    'Authorization': authorization,
+    }
+
+    res = requests.get(server_url + '/v1/orders', params=params, headers=headers).json()
+
+    return res[0]
+
+
 def order_cancel(order_uuid):
     params = {
     'uuid': order_uuid
