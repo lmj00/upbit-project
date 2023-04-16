f3d116dfc15d77014ab2177a1e05bfe98b04130e
이민진 <e.minjin00@gmail.com>
2023-04-17 03:10:51 +0900

    Fix: 원화 코인을 온전히 받아오지 못하는 버그 수정

--- coin/coin.py
+++ coin/coin.py
@@ -4,6 +4,7 @@ import websockets
 import json 
 import requests
 import json 
+import time
 
 
 def get_market():
@@ -25,51 +26,53 @@ def get_market():
 
 
 async def get_ticker():
-    async with websockets.connect('wss://api.upbit.com/websocket/v1') as websocket:
-        request_ticker = [
-            {"ticket":"ticket"},
-            {
-                "type":"ticker",
-                "codes": get_market()
-            }
-        ]
+    while True:
+        async with websockets.connect('wss://api.upbit.com/websocket/v1') as websocket:
+            request_ticker = [
+                {"ticket":"ticket"},
+                {
+                    "type":"ticker",
+                    "codes": get_market()
+                }
+            ]
 
-        await websocket.send(json.dumps(request_ticker))
-
-        for _ in range(len(get_market())):
-
-            recv = await websocket.recv()
-            recv_obj = json.loads(recv)
-            
-            Ticker.objects.create(
-                type = recv_obj['type'],
-                code = recv_obj['code'],
-                opening_price = recv_obj['opening_price'],
-                high_price = recv_obj['high_price'],
-                low_price = recv_obj['low_price'],
-                trade_price = recv_obj['trade_price'],
-                prev_closing_price = recv_obj['prev_closing_price'],
-                change = recv_obj['change'],
-                change_price =  recv_obj['change_price'],
-                signed_change_price = recv_obj['signed_change_price'],
-                trade_volume = recv_obj['trade_volume'],
-                acc_trade_volume = recv_obj['acc_trade_volume'],
-                acc_trade_volume_24h = recv_obj['acc_trade_volume_24h'],
-                acc_trade_price = recv_obj['acc_trade_price'],
-                acc_trade_price_24h	= recv_obj['acc_trade_price_24h'],
-                trade_date = recv_obj['trade_date'],
-                trade_time = recv_obj['trade_time'],
-                trade_timestamp = recv_obj['trade_timestamp'],
-                ask_bid = recv_obj['ask_bid'],
-                acc_ask_volume = recv_obj['acc_ask_volume'],
-                acc_bid_volume = recv_obj['acc_bid_volume'], 
-                highest_52_week_price = recv_obj['highest_52_week_price'],
-                highest_52_week_date = recv_obj['highest_52_week_date'],
-                lowest_52_week_price = recv_obj['lowest_52_week_price'],
-                lowest_52_week_date = recv_obj['lowest_52_week_date'],
-                market_state = recv_obj['market_state'],
-                timestamp = recv_obj['timestamp']
+            await websocket.send(json.dumps(request_ticker))
+        
+            for _ in range(len(get_market())):
+                recv = await websocket.recv()
+                recv_obj = json.loads(recv)
+                
+                Ticker.objects.create(
+                    type = recv_obj['type'],
+                    code = recv_obj['code'],
+                    opening_price = recv_obj['opening_price'],
+                    high_price = recv_obj['high_price'],
+                    low_price = recv_obj['low_price'],
+                    trade_price = recv_obj['trade_price'],
+                    prev_closing_price = recv_obj['prev_closing_price'],
+                    change = recv_obj['change'],
+                    change_price =  recv_obj['change_price'],
+                    signed_change_price = recv_obj['signed_change_price'],
+                    trade_volume = recv_obj['trade_volume'],
+                    acc_trade_volume = recv_obj['acc_trade_volume'],
+                    acc_trade_volume_24h = recv_obj['acc_trade_volume_24h'],
+                    acc_trade_price = recv_obj['acc_trade_price'],
+                    acc_trade_price_24h	= recv_obj['acc_trade_price_24h'],
+                    trade_date = recv_obj['trade_date'],
+                    trade_time = recv_obj['trade_time'],
+                    trade_timestamp = recv_obj['trade_timestamp'],
+                    ask_bid = recv_obj['ask_bid'],
+                    acc_ask_volume = recv_obj['acc_ask_volume'],
+                    acc_bid_volume = recv_obj['acc_bid_volume'], 
+                    highest_52_week_price = recv_obj['highest_52_week_price'],
+                    highest_52_week_date = recv_obj['highest_52_week_date'],
+                    lowest_52_week_price = recv_obj['lowest_52_week_price'],
+                    lowest_52_week_date = recv_obj['lowest_52_week_date'],
+                    market_state = recv_obj['market_state'],
+                    timestamp = recv_obj['timestamp']
             )
+
+            time.sleep(1)
 
 
 def get_top_trade_price_coin():
