40ab36d94f67e871a2d98b1dab0ed1153e790afb
이민진 <e.minjin00@gmail.com>
2023-04-17 03:47:28 +0900

    Feat: 소켓이 끊기면 재연결

--- coin/consumers.py
+++ coin/consumers.py
@@ -12,24 +12,31 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
-    
-        while True:
-            coin_dic = {}
 
-            for ttpc in get_top_trade_price_coin():
-                coin_dic[ttpc.code] = ttpc.trade_price
+        try:
+            while True:
+                coin_dic = {}
+                
+                for ttpc in get_top_trade_price_coin():
+                    coin_dic[ttpc.code] = ttpc.trade_price
 
-            await self.channel_layer.group_send(
-                self.room_group_name, {
-                    "type": "ticker", 
-                    "coin": coin_dic
-                }
-            )
-            
-            await self.send(text_data=json.dumps({"coin_dic": coin_dic}))
-            
-            time.sleep(0.001)
+
+                await self.channel_layer.group_send(
+                    self.room_group_name, {
+                        "type": "ticker", 
+                        "coin": coin_dic
+                    }
+                )
+                
+                await self.send(text_data=json.dumps({"coin_dic": coin_dic}))
+                
+                time.sleep(1)
+        except:
+            print("재연결")
+            await self.connect()
 
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_discard(
+            self.room_group_name, self.channel_name
+        )
