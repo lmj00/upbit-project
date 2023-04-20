df1d8bb6fb8b5e98c1035a8cc5fa40c365c0fb51
이민진 <e.minjin00@gmail.com>
2023-04-20 18:02:02 +0900

    Fix: consumers 재연결 코드 삭제
    이전 코드에서 self.connect()로 재연결하는 건 정상 작동하는 코드가 아님

--- coin/consumers.py
+++ coin/consumers.py
@@ -1,9 +1,7 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .coin import get_top_trade_price_coin
 
-import time
 import json
-
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -13,29 +11,20 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
-        try:
-            while True:
-                coin_dic = {}
-                
-                for ttpc in get_top_trade_price_coin():
-                    coin_dic[ttpc.code] = ttpc.trade_price
+        while True:
+            coin_dic = {}
+    
+            for ttpc in get_top_trade_price_coin():
+                coin_dic[ttpc.code] = ttpc.trade_price
 
-
-                await self.channel_layer.group_send(
-                    self.room_group_name, {
-                        "type": "ticker", 
-                        "coin": coin_dic
-                    }
-                )
-                
-                await self.send(text_data=json.dumps({"coin_dic": coin_dic}))
-                
-                time.sleep(1)
-        except:
-            print("재연결")
-            await self.connect()
-
-
+            await self.channel_layer.group_send(
+                self.room_group_name, {
+                    "type": "ticker", 
+                    "coin": coin_dic
+                }
+            )
+            
+            await self.send(text_data=json.dumps({"coin_dic": coin_dic}))
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
