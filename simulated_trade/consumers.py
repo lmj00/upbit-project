2eabdafd4cad934ec980088a174ed075826251c5
이민진 <e.minjin00@gmail.com>
2023-05-05 02:57:39 +0900

    Feat: smlTradeConsumer의 코인들을 24시간 누적 거래대금으로 정렬

--- simulated_trade/consumers.py
+++ simulated_trade/consumers.py
@@ -6,7 +6,6 @@ from coin.models import Ticker
 
 import json
 
-
 class smlTradeConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["sml_trade"]
@@ -18,19 +17,22 @@ class smlTradeConsumer(AsyncWebsocketConsumer):
         while True:
             length = len(get_krw_market_list()) 
             ticker_qs = Ticker.objects.order_by('-id')[:length]
-            ticker_dic = {}
+            ticker_ls = []
 
             for ts in ticker_qs:
-                ticker_dic[ts.code] = model_to_dict(ts)
+                ticker_ls.append(model_to_dict(ts))
+
 
             await self.channel_layer.group_send(
-                self.room_group_name, {
+                self.room_group_name, { 
                     "type": "ticker", 
-                    "ticker_dic": ticker_dic
+                    "ticker_ls": ticker_ls
                 }
             )
-            await self.send(text_data=json.dumps(ticker_dic))
 
+            ticker_ls.sort(key=lambda x:x['acc_trade_price_24h'], reverse=True)
+            
+            await self.send(text_data=json.dumps(ticker_ls))
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
