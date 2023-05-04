f172c0628f43f613741ee2862148e7f36c945fef
이민진 <e.minjin00@gmail.com>
2023-05-04 16:43:12 +0900

    Feat: smlTradeConsumer 추가

---
+++ simulated_trade/consumers.py
@@ -0,0 +1,38 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+from django.forms.models import model_to_dict
+
+from coin.coin import get_krw_market_list
+from coin.models import Ticker
+
+import json
+
+
+class smlTradeConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["sml_trade"]
+        self.room_group_name = "sml_trade_%s" % self.room_name
+
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
+
+        while True:
+            length = len(get_krw_market_list()) 
+            ticker_qs = Ticker.objects.order_by('-id')[:length]
+            ticker_dic = {}
+
+            for ts in ticker_qs:
+                ticker_dic[ts.code] = model_to_dict(ts)
+
+            await self.channel_layer.group_send(
+                self.room_group_name, {
+                    "type": "ticker", 
+                    "ticker_dic": ticker_dic
+                }
+            )
+            await self.send(text_data=json.dumps(ticker_dic))
+
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name, self.channel_name
+        )
