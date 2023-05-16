9ab8dcb894fd0dd080930553f6881743b98bf9b5
이민진 <e.minjin00@gmail.com>
2023-05-16 19:19:06 +0900

    Refactor: smlTradeConsumer 메서드 분리

--- simulated_trade/consumers.py
+++ simulated_trade/consumers.py
@@ -4,37 +4,40 @@ from django.forms.models import model_to_dict
 from coin.coin import get_krw_market_list
 from coin.models import Ticker
 
+import asyncio
 import json
 
-class smlTradeConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["sml_trade"]
-        self.room_group_name = "sml_trade_%s" % self.room_name
 
+class smlTradeConsumer(AsyncWebsocketConsumer):
+    room_group_name = 'coin_group'
+
+    async def connect(self):
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
-
-        while True:
-            length = len(get_krw_market_list()) 
-            ticker_qs = Ticker.objects.order_by('-id')[:length]
-            ticker_ls = []
-
-            for ts in ticker_qs:
-                ticker_ls.append(model_to_dict(ts))
-
-
-            await self.channel_layer.group_send(
-                self.room_group_name, { 
-                    "type": "ticker", 
-                    "ticker_ls": ticker_ls
-                }
-            )
-
-            ticker_ls.sort(key=lambda x:x['acc_trade_price_24h'], reverse=True)
-            
-            await self.send(text_data=json.dumps(ticker_ls))
+        await self.send_ticker()
+    
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
+
+
+    async def send_ticker(self):
+        length = len(get_krw_market_list()) 
+
+        while True:
+            ticker_qs = Ticker.objects.order_by('-id')[:length]
+            ticker_ls = [model_to_dict(ts) for ts in ticker_qs]
+
+            await self.channel_layer.group_send(
+                self.room_group_name, { 
+                    "ticker_ls": ticker_ls
+                }
+            )
+
+            ticker_ls.sort(key=lambda x:x['acc_trade_price_24h'], reverse=True)
+
+            await self.send(text_data=json.dumps(ticker_ls))
+
+            await asyncio.sleep(1)
