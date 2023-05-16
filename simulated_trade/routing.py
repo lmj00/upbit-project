6ea92262d53a212d4a3afca0337c9e3ddb3eb9c5
이민진 <e.minjin00@gmail.com>
2023-05-16 22:40:19 +0900

    Feat: sml_trade 소켓 URL 변경

--- simulated_trade/routing.py
+++ simulated_trade/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/sml_trade/(?P<sml_trade>\w+)/$", consumers.smlTradeConsumer.as_asgi()),
+    re_path(r"ws/(?P<sml_trade>\w+)/$", consumers.smlTradeConsumer.as_asgi()),
 ]
