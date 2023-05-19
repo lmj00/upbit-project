cd28268161cd0e44e5ccf10ed6a6d7e7eadd6d26
이민진 <e.minjin00@gmail.com>
2023-05-20 04:57:14 +0900

    feat: smlAccount model 생성

--- simulated_trade/models.py
+++ simulated_trade/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class smlAccount(models.Model):
+    currency = models.CharField(max_length=20)
+    balance = models.FloatField()
+    avg_buy_price = models.FloatField()
+    unit_currency = models.CharField(max_length=5)
