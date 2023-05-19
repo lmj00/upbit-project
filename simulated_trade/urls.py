51a6aef997697bf1639c225627e5dcf8cceb77d0
이민진 <e.minjin00@gmail.com>
2023-05-20 07:11:36 +0900

    feat: sml_trade url 추가

--- simulated_trade/urls.py
+++ simulated_trade/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =  [
-    path('', views.index, name='sml_index')
+    path('', views.index, name='sml-index'),
+    path('order/bid', views.order_bid, name='sml-order-bid')
 ]
