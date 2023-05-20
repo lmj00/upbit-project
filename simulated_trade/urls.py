e967060bdf2153035c0df9788dcf2e7e1bd19a80
이민진 <e.minjin00@gmail.com>
2023-05-21 07:45:38 +0900

    feat: sml_trade 매도 url 추가

--- simulated_trade/urls.py
+++ simulated_trade/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns =  [
     path('', views.index, name='sml-index'),
-    path('order/bid', views.order_bid, name='sml-order-bid')
+    path('order/bid', views.order_bid, name='sml-order-bid'),
+    path('order/ask', views.order_ask, name='sml-order-ask')
+    
 ]
