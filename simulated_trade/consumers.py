870e2f6922a300fbcc2ef90d6cb3ecbf2d0b84ea
이민진 <e.minjin00@gmail.com>
2023-06-04 07:17:51 +0900

    refactor: send_ticker에 있던 send_account_coin 함수 분리

--- simulated_trade/consumers.py
+++ simulated_trade/consumers.py
@@ -1,7 +1,7 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.forms.models import model_to_dict
 
-from coin.coin import get_krw_codes_list, get_coin_snapshot, get_kr_name_dic
+from coin.coin import get_krw_codes_list
 
 from coin.models import Ticker
 from .models import smlAccount
@@ -9,21 +9,16 @@ from .models import smlAccount
 import asyncio
 import json
 
-
 class smlTradeConsumer(AsyncWebsocketConsumer):
-
     async def connect(self):
         # self.room_group_name = 'coin_group'
 
         # await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
-        await self.send_ticker()
-    
-
-    # async def disconnect(self, close_code):
-    #     await self.channel_layer.group_discard(
-    #         self.room_group_name, self.channel_name
-    #     )
+        await asyncio.gather(
+            self.send_ticker(),
+            self.send_account_coin()
+        )
 
 
     async def send_ticker(self):
@@ -46,37 +41,44 @@ class smlTradeConsumer(AsyncWebsocketConsumer):
                 "value": ticker_ls
             }))
 
-            await self.send_account()
             await asyncio.sleep(1)
 
 
-    async def send_account(self):
-        sm_ac_objects = smlAccount.objects.all()
-        sml_account_ls = []
-        
-        for coin in sm_ac_objects:
-            code = coin.currency + "-" + coin.unit_currency
-            dic = {}
+    async def send_account_coin(self):
+        length = len(get_krw_codes_list()) 
 
-            if code != 'KRW-KRW':
-                gcs = get_coin_snapshot(code)
-                name = get_kr_name_dic()[code]
+        while True:
+            sm_ac_objects = smlAccount.objects.all()
+            sml_account_ls = []
+            gcs = None
 
-                rate_of_return = round((gcs['trade_price'] - coin.avg_buy_price) / coin.avg_buy_price * 100, 2)
-                amount_money = coin.balance * coin.avg_buy_price
-                valuation_amount = amount_money + (amount_money * rate_of_return / 100) 
+            for coin in sm_ac_objects:
+                code = coin.currency + "-" + coin.unit_currency
+                dic = {}
 
-                dic['name'] = name
-                dic['balance'] = coin.balance
-                dic['avg_buy_price'] = coin.avg_buy_price
-                dic['amount_money'] = amount_money
-                dic['rate_of_return'] = rate_of_return
-                dic['valuation_amount'] = valuation_amount
+                if code != 'KRW-KRW':
+                    ticker_qs = Ticker.objects.order_by('-id')[:length]
 
-                sml_account_ls.append(dic)
+                    for tqs in ticker_qs:
+                        if code == tqs.code:
+                            gcs = tqs
 
+                    rate_of_return = round((gcs.trade_price - coin.avg_buy_price) / coin.avg_buy_price * 100, 2)
+                    amount_money = coin.balance * coin.avg_buy_price
+                    valuation_amount = amount_money + (amount_money * rate_of_return / 100) 
 
-        await self.send(text_data=json.dumps({
-            "type": "sml_account",
-            "value": sml_account_ls
-        }))
+                    dic['name'] = gcs.name
+                    dic['balance'] = coin.balance
+                    dic['avg_buy_price'] = coin.avg_buy_price
+                    dic['amount_money'] = amount_money
+                    dic['rate_of_return'] = rate_of_return
+                    dic['valuation_amount'] = valuation_amount
+
+                    sml_account_ls.append(dic)
+
+            await self.send(text_data=json.dumps({
+                "type": "sml_account",
+                "value": sml_account_ls
+            }))
+
+            await asyncio.sleep(1)
