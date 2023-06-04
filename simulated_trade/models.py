74631ebc1c8b92a34e61bf747de9b3d47791e33f
이민진 <e.minjin00@gmail.com>
2023-06-05 08:07:08 +0900

    feat: sml history 모델 생성

--- simulated_trade/models.py
+++ simulated_trade/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from decimal import Decimal
 
 # Create your models here.
 class Account(models.Model):
@@ -6,3 +7,12 @@ class Account(models.Model):
     balance = models.FloatField()
     avg_buy_price = models.FloatField()
     unit_currency = models.CharField(max_length=5)
+
+
+class History(models.Model):
+    side = models.CharField(max_length=3)
+    market = models.CharField(max_length=20)
+    price = models.DecimalField(max_digits=40, decimal_places=20, default=Decimal('0.0'))
+    created_at = models.DateField(auto_now_add=True)
+    volume = models.DecimalField(max_digits=60, decimal_places=20, default=Decimal('0.0'))
+    paid_fee = models.DecimalField(max_digits=40, decimal_places=20, default=Decimal('0.0'))
