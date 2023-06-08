74c81415d48533f0cbe4d0e1830b09e4d15056f7
이민진 <e.minjin00@gmail.com>
2023-06-09 07:16:59 +0900

    feat: History 모델 DateTimeField로 변경

--- simulated_trade/models.py
+++ simulated_trade/models.py
@@ -13,6 +13,6 @@ class History(models.Model):
     side = models.CharField(max_length=3)
     market = models.CharField(max_length=20)
     price = models.DecimalField(max_digits=40, decimal_places=20, default=Decimal('0.0'))
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     volume = models.DecimalField(max_digits=60, decimal_places=20, default=Decimal('0.0'))
     paid_fee = models.DecimalField(max_digits=40, decimal_places=20, default=Decimal('0.0'))
