6f533053ed7a1a008be2480a077c53e96fb45ea0
이민진 <e.minjin00@gmail.com>
2023-06-26 03:58:25 +0900

    refactor: get_ticker while 삭제

--- coin/coin.py
+++ coin/coin.py
@@ -39,58 +39,56 @@ def get_kr_name_dic():
 
 
 async def get_ticker():
-    while True:
-        async with websockets.connect('wss://api.upbit.com/websocket/v1') as websocket:
-            request_ticker = [
-                {"ticket":"ticket"},
-                {
-                    "type":"ticker",
-                    "codes": get_krw_codes_list()
-                }
-            ]
+    async with websockets.connect('wss://api.upbit.com/websocket/v1') as websocket:
+        request_ticker = [
+            {"ticket":"ticket"},
+            {
+                "type":"ticker",
+                "codes": get_krw_codes_list()
+            }
+        ]
 
-            await websocket.send(json.dumps(request_ticker))
-            await websocket.ping()
+        await websocket.send(json.dumps(request_ticker))
 
-            kr_name_dic = get_kr_name_dic()
+        kr_name_dic = get_kr_name_dic()
 
-            for _ in range(len(get_krw_codes_list())):
-                recv = await websocket.recv()
-                recv_obj = json.loads(recv)
+        for _ in range(len(get_krw_codes_list())):
+            recv = await websocket.recv()
+            recv_obj = json.loads(recv)
 
-                code = recv_obj['code']
-                name = kr_name_dic.get(code)
+            code = recv_obj['code']
+            name = kr_name_dic.get(code)
 
-                Ticker.objects.create(
-                    type = recv_obj['type'],
-                    code = code,
-                    name = name,
-                    opening_price = recv_obj['opening_price'],
-                    high_price = recv_obj['high_price'],
-                    low_price = recv_obj['low_price'],
-                    trade_price = recv_obj['trade_price'],
-                    prev_closing_price = recv_obj['prev_closing_price'],
-                    change = recv_obj['change'],
-                    change_price =  recv_obj['change_price'],
-                    signed_change_price = recv_obj['signed_change_price'],
-                    trade_volume = recv_obj['trade_volume'],
-                    acc_trade_volume = recv_obj['acc_trade_volume'],
-                    acc_trade_volume_24h = recv_obj['acc_trade_volume_24h'],
-                    acc_trade_price = recv_obj['acc_trade_price'],
-                    acc_trade_price_24h	= recv_obj['acc_trade_price_24h'],
-                    trade_date = recv_obj['trade_date'],
-                    trade_time = recv_obj['trade_time'],
-                    trade_timestamp = recv_obj['trade_timestamp'],
-                    ask_bid = recv_obj['ask_bid'],
-                    acc_ask_volume = recv_obj['acc_ask_volume'],
-                    acc_bid_volume = recv_obj['acc_bid_volume'], 
-                    highest_52_week_price = recv_obj['highest_52_week_price'],
-                    highest_52_week_date = recv_obj['highest_52_week_date'],
-                    lowest_52_week_price = recv_obj['lowest_52_week_price'],
-                    lowest_52_week_date = recv_obj['lowest_52_week_date'],
-                    market_state = recv_obj['market_state'],
-                    timestamp = recv_obj['timestamp']
-            )
+            Ticker.objects.create(
+                type = recv_obj['type'],
+                code = code,
+                name = name,
+                opening_price = recv_obj['opening_price'],
+                high_price = recv_obj['high_price'],
+                low_price = recv_obj['low_price'],
+                trade_price = recv_obj['trade_price'],
+                prev_closing_price = recv_obj['prev_closing_price'],
+                change = recv_obj['change'],
+                change_price =  recv_obj['change_price'],
+                signed_change_price = recv_obj['signed_change_price'],
+                trade_volume = recv_obj['trade_volume'],
+                acc_trade_volume = recv_obj['acc_trade_volume'],
+                acc_trade_volume_24h = recv_obj['acc_trade_volume_24h'],
+                acc_trade_price = recv_obj['acc_trade_price'],
+                acc_trade_price_24h	= recv_obj['acc_trade_price_24h'],
+                trade_date = recv_obj['trade_date'],
+                trade_time = recv_obj['trade_time'],
+                trade_timestamp = recv_obj['trade_timestamp'],
+                ask_bid = recv_obj['ask_bid'],
+                acc_ask_volume = recv_obj['acc_ask_volume'],
+                acc_bid_volume = recv_obj['acc_bid_volume'], 
+                highest_52_week_price = recv_obj['highest_52_week_price'],
+                highest_52_week_date = recv_obj['highest_52_week_date'],
+                lowest_52_week_price = recv_obj['lowest_52_week_price'],
+                lowest_52_week_date = recv_obj['lowest_52_week_date'],
+                market_state = recv_obj['market_state'],
+                timestamp = recv_obj['timestamp']
+        )
 
 
 
