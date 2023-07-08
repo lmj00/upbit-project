8ff4c0a114c1eb7ff45b03536527bd8107cc12c0
이민진 <e.minjin00@gmail.com>
2023-07-08 09:42:41 +0900

    feat: sml trade 매수, 매도 테스트 케이스 추가

--- simulated_trade/tests.py
+++ simulated_trade/tests.py
@@ -1,3 +1,76 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Account
+
+import json
+
+
+class TestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Account.objects.create(
+            currency='KRW',
+            balance=100_000_000,
+            avg_buy_price=0.0,
+            unit_currency='KRW'
+        )
+
+        Account.objects.create(
+            currency='BTC',
+            balance=1,
+            avg_buy_price=30_000_000,
+            unit_currency='KRW'
+        )
+
+
+    def test_order_bid(self):
+        c = Client()
+        url = reverse('order-bid')
+
+        data = {
+            'in_name': '비트코인',
+            'in_price': 30_000_000,
+            'in_quantity': 1,
+            'in_total': 30_000_000
+        }
+
+        json_data = json.dumps(data)
+
+        response = c.post(
+            url,
+            data=json_data,
+            content_type='application/json',
+        )
+
+        response_data = json.loads(response.content)
+        message = response_data['message']
+
+        assert response.status_code == 200
+        assert message == '매수주문이 완료되었습니다.'
+
+
+    def test_order_ask(self):
+        c = Client()
+        url = reverse('order-ask')
+
+        data = {
+            'in_name': '비트코인',
+            'in_price': 30_000_000,
+            'in_quantity': 1,
+            'in_total': 30_000_000
+        }
+
+        json_data = json.dumps(data)
+
+        response = c.post(
+            url,
+            data=json_data,
+            content_type='application/json',
+        )
+
+        response_data = json.loads(response.content)
+        message = response_data['message']
+
+        assert response.status_code == 200
+        assert message == '매도주문이 완료되었습니다.'
