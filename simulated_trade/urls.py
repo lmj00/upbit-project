34db03e3447f94613c13a739b1b0e87fcd8cb161
이민진 <e.minjin00@gmail.com>
2023-07-08 09:34:24 +0900

    refactor: sml 매수, 매도 url 변경

--- simulated_trade/urls.py
+++ simulated_trade/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns =  [
     path('', views.index, name='sml-index'),
-    path('order/bid', views.order_bid, name='sml-order-bid'),
-    path('order/ask', views.order_ask, name='sml-order-ask')
+    path('order/bid', views.order_bid, name='order-bid'),
+    path('order/ask', views.order_ask, name='order-ask')
     
 ]
