f600b173767d1ff6d0fd24f7e3c300616b3a73f1
이민진 <e.minjin00@gmail.com>
2023-08-22 23:36:49 +0900

    feat: check_bookmark path 추가

--- simulated_trade/urls.py
+++ simulated_trade/urls.py
@@ -5,5 +5,6 @@ urlpatterns =  [
     path('', views.index, name='sml-index'),
     path('order/bid', views.order_bid, name='order-bid'),
     path('order/ask', views.order_ask, name='order-ask'),
-    path('history/<str:code>', views.get_history, name='get-history')
+    path('history/<str:code>', views.get_history, name='get-history'),
+    path('bookmark/<str:code>', views.check_bookmark, name='check-bookmark'),
 ]
